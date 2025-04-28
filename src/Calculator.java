96d6458f0712c082a13967b0d25d3a2ab02889a0
Ghost9841 <asubedi778@gmail.com>
2025-04-28 20:46:20 +0545

    Added Buttons, now need to add functionality to it

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,9 +14,21 @@ public class Calculator {
     Color customBlack = new Color(28,28,28);
     Color customOrange = new Color(255, 149,0);
 
+    String[] buttonsValues = {
+        "AC",  "+/-", "%", "÷",
+        "7","8","9","×",
+        "4","5","6","-",
+        "1","2","3","+",
+        "0", ".", "√", "="
+    };
+
+    String[] rightSymbols = {"÷","×","-","+","="};
+    String[] topSymbols = {"AC", "+/-", "%"};
+
     JFrame frame = new JFrame("Calculator");
     JLabel displayLabel = new JLabel();
     JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
     
     Calculator(){   
         frame.setVisible(true);
@@ -37,5 +49,18 @@ public class Calculator {
         displayPanel.setLayout(new BorderLayout());
         displayPanel.add(displayLabel);
         frame.add(displayPanel,BorderLayout.NORTH);
+
+
+        buttonsPanel.setLayout(new GridLayout(5,4));
+        buttonsPanel.setBackground(customBlack);
+        frame.add(buttonsPanel);
+
+        for (int i = 0; i< buttonsValues.length; i++){
+            JButton button = new JButton();
+            String buttonValue = buttonsValues[i];
+            button.setFont(new Font("Arial", Font.PLAIN,30));
+            button.setText(buttonValue);
+            buttonsPanel.add((button));
+        }
     }
 }   
