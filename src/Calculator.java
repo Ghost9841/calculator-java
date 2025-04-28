79175310268e4441033fbfde1ed1c23ff7692153
Ghost9841 <asubedi778@gmail.com>
2025-04-28 15:18:42 +0545

    Created Java, Added Extension, create calculator app like instance

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+
+public class Calculator {
+    int winWidth = 360;
+    int winHeight = 540;
+
+    JFrame frame = new JFrame("Calculator");
+
+    Calculator(){
+        
+    }
+}
